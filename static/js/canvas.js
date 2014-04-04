2d6c5733ef1f56b0c831c82eda63fa1ed48b019c
KID the Euforia <kid0725@gmail.com>
2014-04-04 18:19:15 +0900

    wip: show pictures and drag'n'drop them, move them from code.

---
+++ static/js/canvas.js
@@ -0,0 +1,169 @@
+$(function(){
+    $('#canvas')
+    .drawImage({
+        layer: true,
+        draggable: true,
+        name: 'ammo',
+        source: './image/ammo.gif',
+    })
+    .drawImage({
+        layer: true,
+        draggable: true,
+        source: './image/breach.gif'
+    })
+    .drawImage({
+        layer: true,
+        draggable: true,
+        source: './image/column.gif'
+    })
+    .drawImage({
+        layer: true,
+        draggable: true,
+        source: './image/come.gif'
+    })
+    .drawImage({
+        layer: true,
+        draggable: true,
+        source: './image/cover.gif'
+    })
+    .drawImage({
+        layer: true,
+        draggable: true,
+        source: './image/dog.gif'
+    })
+    .drawImage({
+        layer: true,
+        draggable: true,
+        source: './image/dontunder.gif'
+    })
+    .drawImage({
+        layer: true,
+        draggable: true,
+        source: './image/door.gif'
+    })
+    .drawImage({
+        layer: true,
+        draggable: true,
+        source: './image/down.gif'
+    })
+    .drawImage({
+        layer: true,
+        draggable: true,
+        source: './image/enemy.gif'
+    })
+    .drawImage({
+        layer: true,
+        draggable: true,
+        source: './image/enter.gif'
+    })
+    .drawImage({
+        layer: true,
+        draggable: true,
+        source: './image/file.gif'
+    })
+    .drawImage({
+        layer: true,
+        draggable: true,
+        source: './image/freeze.gif'
+    })
+    .drawImage({
+        layer: true,
+        draggable: true,
+        source: './image/gas.gif'
+    })
+    .drawImage({
+        layer: true,
+        draggable: true,
+        source: './image/go.gif'
+    })
+    .drawImage({
+        layer: true,
+        draggable: true,
+        source: './image/hostage.gif'
+    })
+    .drawImage({
+        layer: true,
+        draggable: true,
+        source: './image/hurry.gif'
+    })
+    .drawImage({
+        layer: true,
+        draggable: true,
+        source: './image/leader.gif'
+    })
+    .drawImage({
+        layer: true,
+        draggable: true,
+        source: './image/line.gif'
+    })
+    .drawImage({
+        layer: true,
+        draggable: true,
+        source: './image/listen.gif'
+    })
+    .drawImage({
+        layer: true,
+        draggable: true,
+        source: './image/me.gif'
+    })
+    .drawImage({
+        layer: true,
+        draggable: true,
+        source: './image/pistol.gif'
+    })
+    .drawImage({
+        layer: true,
+        draggable: true,
+        source: './image/rally.gif'
+    })
+    .drawImage({
+        layer: true,
+        draggable: true,
+        source: './image/rifle.gif'
+    })
+    .drawImage({
+        layer: true,
+        draggable: true,
+        source: './image/shotgun.gif'
+    })
+    .drawImage({
+        layer: true,
+        draggable: true,
+        source: './image/sniper.gif'
+    })
+    .drawImage({
+        layer: true,
+        draggable: true,
+        source: './image/stop.gif'
+    })
+    .drawImage({
+        layer: true,
+        draggable: true,
+        source: './image/understand.gif'
+    })
+    .drawImage({
+        layer: true,
+        draggable: true,
+        source: './image/vehicle.gif'
+    })
+    .drawImage({
+        layer: true,
+        draggable: true,
+        source: './image/watch.gif'
+    })
+    .drawImage({
+        layer: true,
+        draggable: true,
+        source: './image/wedge.gif'
+    })
+    .drawImage({
+        layer: true,
+        draggable: true,
+        source: './image/window.gif'
+    })
+    .drawImage({
+        layer: true,
+        draggable: true,
+        source: './image/you.gif'
+    });
+});
